7905d8ec174e6f6813600b0b8ce150cd59510ed3
DuyPham59 <pduy99295@gmail.com>
2023-06-30 14:00:50 +0700

    duy

--- app/src/main/java/com/example/appmovie/WacthTheMovie.java
+++ app/src/main/java/com/example/appmovie/WacthTheMovie.java
@@ -49,7 +49,7 @@ public class WacthTheMovie extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_logout)
+                R.id.nav_home,  R.id.nav_logout)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_wacth_the_movie);
@@ -76,9 +76,6 @@ public class WacthTheMovie extends AppCompatActivity {
         startActivity(i);
         finish();
     }
-
-
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_wacth_the_movie);
